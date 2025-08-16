56250b905603886743e796560db77dff7d7e7cef
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2025-08-16 12:49:43 +0530

    Update NestedClass.java

--- Nested Class/NestedClass.java
+++ Nested Class/NestedClass.java
@@ -17,8 +17,8 @@ class Outer{
 
 public class NestedClass{
     public static void main(String args[]){
-       // Outer o = new Outer();
-       // o.outerDisplay();
+       Outer o = new Outer();
+       o.outerDisplay();
         
     }
 }
