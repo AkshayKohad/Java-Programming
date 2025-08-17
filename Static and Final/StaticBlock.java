8ca235f9a8c63d9002d7c886ec4f4c41c616324e
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2025-08-17 21:22:57 +0530

    Create StaticBlock.java

---
+++ Static and Final/StaticBlock.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+class test{
+    static{
+        System.out.println("Block 1");
+    }
+    
+    static{
+        System.out.println("Block 2");
+    }
+}
+public class StaticBlock {
+    public static void main(String[] args) {
+        System.out.println("Main");
+        
+        test t = new test();
+    }
+}
