3f6ec44dfa68378d38b2763db66c1eb1ffbda56a
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2025-08-26 17:35:06 +0530

    Update MultiThreadingThreadClass.java

--- MultiThreading/MultiThreadingThreadClass.java
+++ MultiThreading/MultiThreadingThreadClass.java
@@ -1,6 +1,4 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
+// One way using 2 classes
 class MyThread extends Thread{
     public void run(){
         int i=1;
@@ -14,6 +12,29 @@ class MyThread extends Thread{
 public class MultiThreadingThreadClass {
     public static void main(String[] args) {
         MyThread t = new MyThread();
+        t.start();
+        
+        int i=1;
+        while(true){
+            System.out.println(i+" World");
+            i++;
+        }
+    }
+}
+
+// Other way using only one class
+oublic class MultiThreadingThreadClass extends Thread{
+    
+    public void run(){
+        int i=1;
+        while(true){
+            System.out.println(i+" Hello");
+            i++;
+        }
+    }
+    
+    public static void main(String[] args) {
+        MultiThreadingThreadClass t = new MultiThreadingThreadClass();
         t.start();
         
         int i=1;
