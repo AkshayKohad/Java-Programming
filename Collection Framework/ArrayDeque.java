38614fca8681ca915dd0dc769dfb621f97bb1313
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2025-10-31 16:36:29 +0530

    Create ArrayDeque.java

---
+++ Collection Framework/ArrayDeque.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayDeque<Integer>dq = new ArrayDeque<>();
+        
+        dq.offerLast(10);
+        dq.offerLast(20);
+        dq.offerLast(30);
+        dq.offerLast(40);
+        
+        dq.pollFirst();
+        
+        dq.offerLast(1);
+        dq.offerLast(2);
+        dq.offerLast(3);
+        dq.offerLast(4);
+         
+        dq.forEach((x)->System.out.println(x));
+    }
+}
